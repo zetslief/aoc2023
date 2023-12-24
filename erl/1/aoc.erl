74d17d617f04c59646a50239a5048580896357f8
Konstantin Konstantin <zetslief@protonmail.com>
2023-12-25 00:38:33 +0100

    Splitting lines in erl.

--- erl/1/aoc.erl
+++ erl/1/aoc.erl
@@ -2,14 +2,30 @@
 -export ([main/0]).
 
 main() -> 
-    case file:read_file("data.txt") of
+    Lines = case file:read_file("./../../data/1.txt") of
          {ok, Content} -> split_lines(Content);
          {error, Error} -> io:write(Error)
-    end.
+    end,
+    process_lines(Lines).
+
 
 split_lines(Content) ->
+    split_lines(Content, []).
+
+split_lines(Content, Acc) ->
     case binary:split(Content, <<"\n">>) of
-        [H, T] -> split_chars(H), split_lines(T);
-        [C] -> split_chars(C);
-        [] -> theend
+        [H, T] -> split_chars(H), split_lines(T, [H | Acc]);
+        [<<>>] -> Acc
     end.
+
+process_lines(Lines) ->
+    process_lines(Lines, 0).
+
+process_lines([H | T], Counter) ->
+    split_chars(H),
+    process_lines(T, Counter + 1);
+process_lines([], Counter) ->
+    {ok, Counter}.
+
+split_chars(C) ->
+    io:format("~s~n", [C]).
