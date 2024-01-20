946550b264681b0136d30f22b2484ad7bae1bdf2
Konstantin Konstantin <zetslief@protonmail.com>
2024-01-20 13:59:44 +0100

    Erlang: day 3, parsing numbers.

---
+++ erl/3/main.erl
@@ -0,0 +1,28 @@
+-module(main).
+-export([main/0]).
+
+main() ->
+    {ok, FileContent} = file:read_file("./../../data/3.txt"),
+    {_RowWidth, _} = binary:match(FileContent, <<"\n">>),
+    Nodes = parse(FileContent),
+    io:format("~w~n", [Nodes]).
+
+parse(Content) ->
+    parse(Content, 0, [], []).
+
+parse(<<Number:1/binary, Rest/binary>>, Cursor, Numbers, Symbols)
+  when Number >= <<$0>>, Number =< <<$9>> ->
+    {NextContent, Num} = parse_number(Rest, Number),
+    EndCursor = Cursor + byte_size(Num),
+    parse(NextContent, EndCursor, [{Cursor, EndCursor, binary_to_integer(Num)} | Numbers], Symbols);
+parse(<<_Char:1/binary, Rest/binary>>, Cursor, Numbers, Symbols) ->
+    parse(Rest, Cursor + 1, Numbers, Symbols);
+parse(<<>>, _Cursor, Numbers, Symbols) ->
+    {Numbers, Symbols}.
+
+parse_number(<<Number:1/binary, Rest/binary>>, Acc)
+  when Number >= <<$0>>, Number =< <<$9>> ->
+    parse_number(Rest, <<Acc/binary, Number/binary>>);
+parse_number(Content, Acc) ->
+    {Content, Acc}.
+
